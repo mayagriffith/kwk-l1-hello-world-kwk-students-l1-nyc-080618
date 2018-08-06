1107bd47b84f0281a905a7291bccbdbb24de126f
mayagriffith <mayaegriffith@gmail.com>
2018-08-06 17:45:13 +0000

    automatically backed up by learn

--- strings.rb
+++ strings.rb
@@ -1,7 +1,1 @@
-puts "Hello"
-puts "Hello".upcase
-puts "Hello".downcase
-puts "Hello".reverse
-puts "Hello".swapcase
-puts "Hello".capitalize
-puts "Greetings people".length
+puts "I love Kode with Klossy"
