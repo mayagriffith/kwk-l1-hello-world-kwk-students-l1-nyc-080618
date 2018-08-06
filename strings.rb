7f1e43076aacc3d484bf27f67ee8072a88b3e575
mayagriffith <mayaegriffith@gmail.com>
2018-08-06 17:41:30 +0000

    automatically backed up by learn

--- strings.rb
+++ strings.rb
@@ -1,7 +1,7 @@
 puts "maya"
 puts "maya".upcase
-Puts “Hello”.downcase
-Puts “Hello”.reverse
-Puts “Hello”.swapcase
-Puts “Hello”.capitalize
-Puts “Greetings people”.length
+puts “Hello”.downcase
+puts “Hello”.reverse
+puts “Hello”.swapcase
+puts “Hello”.capitalize
+puts “Greetings people”.length
